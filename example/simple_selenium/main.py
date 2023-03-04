117c1d792a1a6825d571093bada0dde8d49e73d6
includeno <includeno@gmail.com>
2023-03-05 02:00:18 +0800

    添加headless

--- example/simple_selenium/main.py
+++ example/simple_selenium/main.py
@@ -4,18 +4,18 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 service = Service('/tools/chromedriver')
 service.start()
 
-
 # 创建Chrome浏览器实例
-options = webdriver.ChromeOptions()
+options = Options()
+options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--disable-extensions')
 options.add_argument('--disable-gpu')
-#options.add_argument('--headless')
 options.add_argument('blink-settings=imagesEnabled=false')
 options.add_argument('--disable-software-rasterizer')
 
